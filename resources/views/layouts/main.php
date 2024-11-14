390c45de7152b147a6966ef7e2af62af4cb8b515
Jelcoo <53396500+Jelcoo@users.noreply.github.com>
2024-11-14 15:04:22 +0000

    Add auto dark mode

--- resources/views/layouts/main.php
+++ resources/views/layouts/main.php
@@ -10,9 +10,22 @@
         <link rel="stylesheet" href="/assets/css/style.css">
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+        <script src="/assets/js/bootstrap-theme.js"></script>
     </head>
 
     <body>
+        <div class="container">
+            <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+            <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+                <span class="fs-4">PHPDBM</span>
+            </a>
+
+            <ul class="nav nav-pills">
+                <li class="nav-item"><a href="/" class="nav-link active" aria-current="page">Home</a></li>
+                <li class="nav-item"><a href="/login" class="nav-link">Login</a></li>
+            </ul>
+            </header>
+        </div>
         {{content}}
     </body>
 </html>
