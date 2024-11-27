0239b5c6ef846cef91cdf4985b67a29073add859
Jelcoo <53396500+Jelcoo@users.noreply.github.com>
2024-11-27 19:29:00 +0000

    Add database & table search

--- resources/views/pages/database/view.php
+++ resources/views/pages/database/view.php
@@ -1,4 +1,9 @@
 <h1><?php echo $databaseName; ?></h1>
+
+<div class="d-flex align-items-center gap-2">
+    <input class="form-control" id="search" type="text" placeholder="Search..." autofocus>
+</div>
+
 <div class="table-responsive">
     <table class="table table-striped">
         <thead>
@@ -7,11 +12,39 @@
             </tr>
         </thead>
         <tbody>
-            <?php foreach ($databaseTables as $table) { ?>
-                <tr>
-                    <td><a href="/database/<?php echo $databaseName; ?>/<?php echo $table; ?>"><?php echo $table; ?></a></td>
-                </tr>
-            <?php } ?>
         </tbody>
     </table>
 </div>
+
+<script>
+    const databaseName = <?php echo json_encode($databaseName); ?>;
+    const allTables = <?php echo json_encode($databaseTables); ?>;
+    const tbody = document.querySelector('tbody');
+
+    document.addEventListener('DOMContentLoaded', () => {
+        allTables.forEach((table) => {
+            tbody.appendChild(tableToTableRow(table));
+        });
+    });
+
+    const search = document.getElementById('search');
+    search.addEventListener('input', () => {
+        const filteredTables = allTables.filter((table) => table.toLowerCase().includes(search.value.toLowerCase()));
+        tbody.innerHTML = '';
+        filteredTables.forEach((table) => {
+            tbody.appendChild(tableToTableRow(table));
+        });
+    });
+
+    function tableToTableRow(table) {
+        const tr = document.createElement('tr');
+        const td = document.createElement('td');
+        const a = document.createElement('a');
+        a.href = `/database/${databaseName}/${table}`;
+        a.textContent = table;
+        td.appendChild(a);
+        tr.appendChild(td);
+
+        return tr;
+    }
+</script>
--- resources/views/pages/home.php
+++ resources/views/pages/home.php
@@ -1,5 +1,10 @@
 <p>Welcome to the homepage <?php echo $user; ?></p>
 <p>Here are your databases</p>
+
+<div class="d-flex align-items-center gap-2">
+    <input class="form-control" id="search" type="text" placeholder="Search..." autofocus>
+</div>
+
 <div class="table-responsive">
     <table class="table table-striped">
         <thead>
@@ -8,11 +13,38 @@
             </tr>
         </thead>
         <tbody>
-            <?php foreach ($databases as $database) { ?>
-                <tr>
-                    <td><a href="/database/<?php echo $database; ?>"><?php echo $database; ?></a></td>
-                </tr>
-            <?php } ?>
         </tbody>
     </table>
 </div>
+
+<script>
+    const allDatabases = <?php echo json_encode($databases); ?>;
+    const tbody = document.querySelector('tbody');
+
+    document.addEventListener('DOMContentLoaded', () => {
+        allDatabases.forEach((database) => {
+            tbody.appendChild(dbToTableRow(database));
+        });
+    });
+
+    const search = document.getElementById('search');
+    search.addEventListener('input', () => {
+        const filteredDatabases = allDatabases.filter((database) => database.toLowerCase().includes(search.value.toLowerCase()));
+        tbody.innerHTML = '';
+        filteredDatabases.forEach((database) => {
+            tbody.appendChild(dbToTableRow(database));
+        });
+    });
+
+    function dbToTableRow(database) {
+        const tr = document.createElement('tr');
+        const td = document.createElement('td');
+        const a = document.createElement('a');
+        a.href = `/database/${database}`;
+        a.textContent = database;
+        td.appendChild(a);
+        tr.appendChild(td);
+
+        return tr;
+    }
+</script>
